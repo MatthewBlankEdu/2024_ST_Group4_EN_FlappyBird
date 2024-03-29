cc676f10fd61013aeb17bbc399e76a7e077bebd2
Mateusz Pusty <mateusz.pusty@cdv.pl>
2024-03-29 17:55:15 +0100

    Moving Ground

--- FlappyBird/Assets/MoveController.cs
+++ FlappyBird/Assets/MoveController.cs
@@ -8,14 +8,19 @@ public class MoveController : MonoBehaviour
     public float XDespawnPoint;
     public float XSpawnPoint;
 
+    public bool ShouldRandomizeY;
+
     public float YMin;
     public float YMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(transform.position.x,
-            Random.Range(YMin, YMax), transform.position.z);
+        if (ShouldRandomizeY)
+        {
+            transform.position = new Vector3(transform.position.x,
+                Random.Range(YMin, YMax), transform.position.z);
+        }
     }
 
     // Update is called once per frame
@@ -23,11 +28,19 @@ public class MoveController : MonoBehaviour
     {
         transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime,
             transform.position.y, transform.position.z);
-    
-        if(transform.position.x < XDespawnPoint)
+
+        if (transform.position.x < XDespawnPoint)
         {
-            transform.position = new Vector3(XSpawnPoint, 
-                Random.Range(YMin, YMax), transform.position.z);
+            if (ShouldRandomizeY)
+            {
+                transform.position = new Vector3(XSpawnPoint,
+                    Random.Range(YMin, YMax), transform.position.z);
+            }
+            else
+            {
+                transform.position = new Vector3(XSpawnPoint,
+                    transform.position.y, transform.position.z);
+            }
         }
     }
 }
