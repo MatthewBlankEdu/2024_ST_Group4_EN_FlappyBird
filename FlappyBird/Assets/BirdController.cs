d3bc7a4211ee8a8f87b6bea6a36c88b0ac6da8aa
Mateusz Pusty <mateusz.pusty@cdv.pl>
2024-04-05 17:17:12 +0200

    Project update

--- FlappyBird/Assets/BirdController.cs
+++ FlappyBird/Assets/BirdController.cs
@@ -1,9 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
     public Vector2 JumpForce;
+    public float MaxVelocity;
     public Rigidbody2D Rigidbody2D;
+    public TextMeshProUGUI PointsText;
+    public GameObject GameOverScreen;
+
+    public int Points;
+    public static bool GameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -14,9 +21,32 @@ public class BirdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (GameOver)
+            return;
+
         if(Input.GetButtonDown("Jump"))
         {
+            Rigidbody2D.velocity = Vector2.zero;
             Rigidbody2D.AddForce(JumpForce, ForceMode2D.Impulse);
         }
+
+        if (Rigidbody2D.velocity.y > MaxVelocity)
+            Rigidbody2D.velocity = new Vector2(0, MaxVelocity);
+
+        PointsText.text = Points.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("PointZone"))
+        {
+            Points++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameOverScreen.SetActive(true);
+        GameOver = true;
     }
 }
--- FlappyBird/Assets/MoveController.cs
+++ FlappyBird/Assets/MoveController.cs
@@ -8,12 +8,12 @@ public class MoveController : MonoBehaviour
     public float XDespawnPoint;
     public float XSpawnPoint;
 
-    public bool ShouldRandomizeY;
 
     public float YMin;
     public float YMax;
 
-    // Start is called before the first frame update
+    public bool ShouldRandomizeY;
+
     void Start()
     {
         if (ShouldRandomizeY)
@@ -22,10 +22,12 @@ public class MoveController : MonoBehaviour
                 Random.Range(YMin, YMax), transform.position.z);
         }
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        if (BirdController.GameOver)
+            return;
+
         transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime,
             transform.position.y, transform.position.z);
 
